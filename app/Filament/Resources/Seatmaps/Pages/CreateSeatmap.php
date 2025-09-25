206a70dcd7227bf2c91c424f0e9471ac59445415
Umar Farooq Nadeem <dev.omar561@gmail.com>
2025-09-26 03:11:23 +0500

    Latest

--- app/Filament/Resources/Seatmaps/Pages/CreateSeatmap.php
+++ app/Filament/Resources/Seatmaps/Pages/CreateSeatmap.php
@@ -4,8 +4,21 @@ namespace App\Filament\Resources\Seatmaps\Pages;
 
 use App\Filament\Resources\Seatmaps\SeatmapResource;
 use Filament\Resources\Pages\CreateRecord;
+use Livewire\Attributes\On;
 
 class CreateSeatmap extends CreateRecord
 {
     protected static string $resource = SeatmapResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            \App\Livewire\SeatmapFormContainer::class
+        ];
+    }
 }
---
+++ resources/views/livewire/seatmap-form-container.blade.php
@@ -0,0 +1,12 @@
+<div class="mt-4 p-6 bg-white rounded-lg shadow">
+    <div>
+        <h3 class="text-lg font-medium leading-6 text-gray-900">
+            Seatmap Container
+        </h3>
+        <div class="mt-2">
+            <p class="text-sm text-gray-500">
+                This is a custom container below the seatmap form.
+            </p>
+        </div>
+    </div>
+</div>
