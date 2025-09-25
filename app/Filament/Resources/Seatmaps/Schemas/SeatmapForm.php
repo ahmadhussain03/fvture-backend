fa57050c16251e8af6d8372e354a402acbd295bc
Umar Farooq Nadeem <dev.omar561@gmail.com>
2025-09-26 00:05:53 +0500

    Update SeatmapForm.php

--- app/Filament/Resources/Seatmaps/Schemas/SeatmapForm.php
+++ app/Filament/Resources/Seatmaps/Schemas/SeatmapForm.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\Seatmaps\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class SeatmapForm
@@ -12,17 +15,34 @@ class SeatmapForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                Textarea::make('background_url')
-                    ->required()
-                    ->columnSpanFull(),
-                TextInput::make('map_width')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('map_height')
-                    ->required()
-                    ->numeric(),
+                Tabs::make('Seatmap Details')
+                    ->tabs([
+                        Tabs\Tab::make('Details')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('map_width')
+                                    ->required()
+                                    ->numeric(),
+                                TextInput::make('map_height')
+                                    ->required()
+                                    ->numeric(),
+                            ]),
+                        Tabs\Tab::make('Background Image')
+                            ->schema([
+                                FileUpload::make('background_url')
+                                    ->label('Background Image')
+                                    ->image()
+                                    ->disk('s3')
+                                    ->directory('seatmaps/backgrounds')
+                                    ->visibility('public')
+                                    ->acceptedFileTypes(['image/jpeg', 'image/png', 'image/gif', 'image/webp'])
+                                    ->maxSize(5120)
+                                    ->getUploadedFileNameForStorageUsing(fn (string $file) => (string) str($file)->prepend(time() . '-'))
+                                    ->moveFiles()
+                                    ->required(),
+                            ]),
+                    ]),
             ]);
     }
 }
