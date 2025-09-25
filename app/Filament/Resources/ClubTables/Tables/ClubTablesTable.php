7ea05a44ff2512edcc82d0c0c5bf54d9f06042b6
Umar Farooq Nadeem <dev.omar561@gmail.com>
2025-09-25 20:19:52 +0500

    ClubTable CRUD Done

--- app/Filament/Resources/ClubTables/Tables/ClubTablesTable.php
+++ app/Filament/Resources/ClubTables/Tables/ClubTablesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ClubTables\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -37,6 +38,7 @@ class ClubTablesTable
                 //
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
             ])
             ->toolbarActions([
